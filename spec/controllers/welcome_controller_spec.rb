dd02666f2bb21474e489e7eb4020abb80162fb06
Eljam Russell <eljamrussell@gmail.com>
2017-07-29 13:34:10 -0400

    assignment 12 completed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,17 +4,23 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
   describe "GET index" do
     it "renders the index template" do
-#call the index method of WelcomeController
+      #call the index method of WelcomeController
       get :index
-# expect the controller's response to render the index template
+      # expect the controller's response to render the index template
       expect(response).to render_template("index")
     end
   end
-  
+
   describe "GET about" do
-     it "renders the about template" do
-       get :about
-       expect(response).to render_template("about")
-     end
-   end
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template("about")
+    end
+  end
+  describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+      expect(response).to render_template("faq")
+    end
+  end
 end
