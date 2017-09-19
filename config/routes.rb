643b4176c8f998a361b5c2088c901c2d9e1aea07
Eljam Russell <eljamrussell@gmail.com>
2017-09-19 19:13:25 -0400

    checkpoint 25 completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   # resources :posts
   # changed resources to show passing resources :posts block to resources :topics block for nesting
   resources :topics do
-
     resources :posts, except: [:index]
   end
 
   #create new and create actions, only hash key prevents unnec. route code
   resources :users, only:[:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
 
   # remove the get "welcome/index" because the root is declared as index
   # modify the about route to allow users to visit/about rather than /welcome/about
