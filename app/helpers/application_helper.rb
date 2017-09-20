2cbb0009b546f070a2ecf248ec78a24ea6b9f392
Eljam Russell <eljamrussell@gmail.com>
2017-09-19 22:23:57 -0400

    assignment 25 completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
   # define the method, pass array of errors, code block as a proc
   def form_group_tag(errors, &block)
-     css_class = 'form-group'
-     css_class << ' has-error' if errors.any?
-     # call the content _tag helper method, pass symbol, code block as a proc, and options hash
-     # to create a symbol-specified HTML tag with block contents
-     content_tag :div, capture(&block), class: css_class
-   end
- end
+    css_class = 'form-group'
+    css_class << ' has-error' if errors.any?
+    # call the content _tag helper method, pass symbol, code block as a proc, and options hash
+    # to create a symbol-specified HTML tag with block contents
+    content_tag :div, capture(&block), class: css_class
+  end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
+end
